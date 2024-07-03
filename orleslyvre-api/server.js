65e1d4d06832ff7fa4f2ea42689d1cf9c9b737bc
WilliamMollet <williammdev@gmail.com>
2024-07-03 12:28:11 +0200

    Commit API Initial + API finis

---
+++ orleslyvre-api/server.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const db = require('./db');
+
+// Charger les variables d'environnement
+require('dotenv').config();
+
+// Configuration des options CORS
+const corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200
+};
+
+// Utilise CORS avec des options spécifiques
+app.use(cors(corsOptions));
+// Middleware pour analyser les requêtes JSON
+app.use(express.json());
+
+// Routes pour Category
+app.get('/api/categories', async (req, res) => {
+    try {
+        const rows = await db.query('SELECT * FROM Category');
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post('/api/categories', async (req, res) => {
+    const newCategory = req.body;
+    try {
+        const result = await db.query('INSERT INTO Category (label_cat) VALUES (?)', newCategory.label_cat);
+        res.status(201).json({ id: result[0], ...newCategory });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Routes pour Item
+app.get('/api/items', async (req, res) => {
+    try {
+        const rows = await db.query('SELECT * FROM Item');
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post('/api/items', async (req, res) => {
+    const newItem = req.body;
+    try {
+        const result = await db.query('INSERT INTO Item (name_item, avg_rating_item, desc_item, id_cat) VALUES (?,?,?,?)', [newItem.name_item, newItem.avg_rating_item, newItem.desc_item, newItem.id_cat]);
+        res.status(201).json({ id: result[0], ...newItem });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Routes pour Rating
+app.get('/api/ratings', async (req, res) => {
+    try {
+        const rows = await db.query('SELECT * FROM Rating');
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post('/api/ratings', async (req, res) => {
+    const newRating = req.body;
+    try {
+        const result = await db.query('INSERT INTO Rating (val_rating, name_rating, id_item ) VALUES (?,?,?)', [newRating.val_rating, newRating.name_rating, newRating.id_item]);
+        // Mettre à jour la moyenne des évaluations de l'item
+        await updateAvgRating(newRating.id_item);
+        res.status(201).json({ id: result[0], ...newRating });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Fonction pour mettre à jour la moyenne des évaluations
+async function updateAvgRating(itemId) {
+    try {
+        const ratings = await db.query('SELECT AVG(val_rating) as avg_rating FROM Rating WHERE id_item = ?', [itemId]);
+        console.log(ratings[0].avg_rating);
+        const avgRating = ratings[0].avg_rating;
+        console.log(avgRating);
+        const newAvgRating = await db.query('UPDATE Item SET avg_rating_item = ? WHERE id_item = ?', [avgRating, itemId]);
+        console.log(newAvgRating);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Nouvelle route pour récupérer des items classés par note moyenne
+app.get('/api/items/ranked', async (req, res) => {
+    const { category, limit } = req.query;
+
+    let query = 'SELECT * FROM Item';
+    let queryParams = [];
+
+    if (category) {
+        query += ' WHERE id_cat = ?';
+        queryParams.push(category);
+    }
+
+    query += ' ORDER BY avg_rating_item DESC';
+
+    if (limit) {
+        query += ' LIMIT ?';
+        queryParams.push(parseInt(limit, 10));
+    }
+
+    try {
+        const rows = await db.query(query, queryParams);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
